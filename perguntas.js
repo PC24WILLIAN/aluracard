4669f6b9a319ed7431e9587a0bd59a435cfe3694
PC24WILLIAN <00001114202393SP@al.educacao.sp.gov.br>
2024-11-21 13:47:20 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'adivinhe',
+    'quem e um dos mais viado da sala?',
+    'o Daniel galhardo'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'adivinhe',
+    'Quem e o outro viado? ',
+    'o joao lucas'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'adivinhe',
+    'adivinhe o outro viado? ',
+    'o gabriel ferreira'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'adivinhe ',
+    'quem e a pessoa mais nariguda ?',
+    'o daniel galhardo e o fabio augusto'
 )
